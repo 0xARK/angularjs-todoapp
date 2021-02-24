c9c0000b6a96c2f1c74fe363e53d5e3833374e5e
Matéo Castella <castella.mateo@gmail.com>
2021-02-24 20:57:24 +0100

    setup mixins system

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
+function Mixins() {}
+
+Mixins.prototype.setLSI = function (name, value) {
+    localStorage.setItem(name, value)
+}
+
+Mixins.prototype.getLSI = function (name) {
+    return localStorage.getItem(name)
+}
+
+Mixins.prototype.removeLSI = function (name) {
+    localStorage.removeItem(name)
+}
+
 var app = angular.module("todoapp", ["ngRoute"]);
 
-app.config(function($routeProvider) {
+app.factory("mixins", [function () { return new Mixins()}]);
 
-    'use strict';
+app.config(function($routeProvider) {
 
     $routeProvider
         .when('/' , {
             templateUrl: "partials/list.html",
-            controller: "ListController"
+            controller: "ListTaskController"
         })
         .otherwise({
             redirectTo: "/"
--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,4 +1,7 @@
-app.controller('ListController', function($scope) {
-    $scope.title = "Liste";
-    console.log("List Controller");
+app.controller('ListTaskController', function($scope, mixins) {
+
+    mixins.setLSI('value', 'name')
+    $scope.title = "Todo List";
+    $scope.todos = [{name: "todo1", content: "test"}, {name: "todo2", content: "test"}]
+
 });
